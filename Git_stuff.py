b468233abf049d28b7026729638a75cee5aba129
Lidia <jedrzejewska.lidia.00@gmail.com>
2024-08-30 16:39:00 +0200

    Zmiany - laptop

--- Git_stuff.py
+++ Git_stuff.py
@@ -2,4 +2,12 @@ import tkinter as tk
 
 #create window
 root = tk.Tk()
+root.title("Rob")
+
+label_1 = tk.Label(root,text ="Bye Felis" )
+label_1.grid(column=0, row=0)
+
+button_1 = tk.Button(root,text = "bob")
+button_1.grid(column=1, row=0)
+
 root.mainloop()
